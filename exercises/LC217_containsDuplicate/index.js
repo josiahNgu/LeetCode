b52493197dc160233c140f0d877776014be9c5d3
JosiahNgu <jos81498@gmail.com>
2019-10-28 10:55:05 -0500

    new code

--- exercises/LC217_containsDuplicate/index.js
+++ exercises/LC217_containsDuplicate/index.js
@@ -1,3 +1,19 @@
-const containsDuplicate = nums => {};
+/**
+ * 
+ * look up time of object is O(1), while array is O(N) 
+ */
+const containsDuplicate = nums => {
+    let container ={}
+    for(let i =0; i<nums.length;i++){
+        if(container[nums[i]] === true){
+            return true;
+        }
+        else{
+            container[nums[i]] = true ;
+        }
+    }
+    return false;
+
+};
 
 module.exports = containsDuplicate;
--- exercises/LC238_productExceptSelf/index.js
+++ exercises/LC238_productExceptSelf/index.js
@@ -1,3 +1,22 @@
-const productExceptSelf = nums => {};
+/*
+Time complexity : O(N) - loop through the nums array each time instead of double for loop
+Space complexity: O(N) - size of the nums array
+*/
+const productExceptSelf = nums => {
+    let container = nums.map(n=>1);
+    let product =1;
+    // multiply from the left
+    for(let i =0; i<nums.length;i++){
+        container[i] = product;
+        product = product * nums[i];
+    }
+    product =1;
+    // multiply from the right
+    for(let i =nums.length-1; i>=0;i--){
+        container[i] = product * container[i];
+        product = product * nums[i];
+    }
+    return container;
+};
 
 module.exports = productExceptSelf;
